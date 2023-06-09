9985393421d5372116c3f50ece682850b7952d16
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2023-06-09 11:36:58 +0530

    Test

---
+++ Arrays/ArrayDemo -3.java
@@ -0,0 +1,12 @@
+class ArrayDemo{
+		public static void main(String[] args){
+			String[] enames; //declartion
+			enames = new String[4];
+			
+			System.out.println(enames[0]);
+			System.out.println(enames[1]);
+			System.out.println(enames[2]);
+			System.out.println(enames[3]);
+			System.out.println(enames.length);
+		}
+}
---
+++ Arrays/ArrayDemo.java
@@ -0,0 +1,8 @@
+class ArrayDemo{
+		public static void main(String[] args){
+			String[] enames={"Sonai","Priyanka"};
+			String ename = "Rahul Gandhi";
+			System.out.println(enames.length);
+			System.out.println(ename.length());
+		}
+}
