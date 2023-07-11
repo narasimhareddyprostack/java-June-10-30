630031bb926d40efdc610d21ff66e238a597feb3
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2023-07-11 11:32:19 +0530

    test

---
+++ cfexamples/src/arraylistex/ArrayListOne.java
@@ -0,0 +1,21 @@
+package arraylistex;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListOne {
+
+	public static void main(String[] args) {
+		ArrayList al=new ArrayList();
+		System.out.println(al);//[]
+		al.add(10);
+		al.add("Rahul");
+		al.add(10);
+		al.add(null);
+		System.out.println(al);
+		System.out.println(al.get(0));//10
+		
+
+	}
+
+}
